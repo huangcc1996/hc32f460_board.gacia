a5190a9eb718a33a45459b2fd25ff849bdb3c03d
hcc <1522629366@qq.com>
2026-07-21 14:15:18 +0800

    1.增加ulog。

--- project/applications/main.c
+++ project/applications/main.c
@@ -15,6 +15,17 @@
 #include "board_config.h"
 #include "fal.h"
 #include "spi_flash_sfud.h"
+
+#ifdef LOG_TAG
+#undef LOG_TAG
+#endif
+#ifdef LOG_LVL
+#undef LOG_LVL
+#endif
+
+#define LOG_TAG     "main"     // 该模块对应的标签。不定义时，默认：NO_TAG
+#define LOG_LVL     LOG_LVL_DBG   // 该模块对应的日志输出级别。不定义时，默认：调试级别
+#include <ulog.h>                 // 必须在 LOG_TAG 与 LOG_LVL 下面
 /* defined the LED_GREEN pin: PD4 */
 #define LED_GREEN_PIN GET_PIN(D, 10)
 
@@ -28,6 +39,7 @@ int main(void)
     if (RT_NULL != rt_sfud_flash_probe("norflash0", "spi30")){
         fal_init();
     }
+
     while (1)
     {
         rt_pin_write(LED_GREEN_PIN, PIN_HIGH);
